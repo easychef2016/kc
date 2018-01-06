264ddf34fe52915941297fdaeb3b071b7b216622
easychef2016 <easychef2016@gmail.com>
2018-01-06 13:54:37 -0500

    Add files via upload

---
+++ UserRoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EasyChefDemo.Web.Models
+{
+    public class UserRoleViewModel
+    {
+        public int ID { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+        public virtual RoleViewModel Role { get; set; }
+    }
+}
